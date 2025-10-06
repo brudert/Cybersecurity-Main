7866c0fd96b3410e678c5b04c0e1724899621caf
Bruder <tom.buder@proton.me>
2025-10-06 09:51:33 +0200

    create documentation

--- src/Infrastructure/Freezebee/Configuration.php
+++ src/Infrastructure/Freezebee/Configuration.php
@@ -91,7 +91,7 @@ class Configuration
      *
      * @var string
      */
-    protected $host = 'https://localhost/api';
+    protected $host = 'https://ms-freezebee/api';
 
     /**
      * User agent of the HTTP request, set to "OpenAPI-Generator/{version}/PHP" by default
